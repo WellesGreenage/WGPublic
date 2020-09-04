53a4e4e50f723a80fa564eb7e22968b561b5bb68
WellesGreenage <70760609+WellesGreenage@users.noreply.github.com>
2020-09-04 22:33:51 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,8 +1,43 @@
+<!DOCTYPE html>
 <html>
- <head>
-  <title>Тестируем PHP</title>
- </head>
- <body>
- <?php echo '<p>Привет, мир!</p>'; ?>
- </body>
+
+	<head>
+		<title>My site</title>
+	</head>
+
+
+
+	<body>
+		<?php
+		echo 'Hello, world!';//Вывод
+		?></br>
+		<?php
+		$a = 100;
+		echo $a;
+		$b = array(1, 2, 3, 4, 5);
+		?></br>
+		<?php
+		echo $b[2];
+		?>
+		<?php
+			$w = 'Rac';
+			$e = 'War';
+			echo $w.' '.$e;
+		?><br>
+		<?php
+			$w= 5;
+			$t = 7;
+			if (2+2 == 5){
+				echo $w;
+			} else 
+			{ echo $t;
+		};
+		?>
+		<?php
+		for($i = 0; $i <= 10; $i++)
+		{echo 'Hello<br>';}
+		?>
+
+	</body>
+
 </html>
