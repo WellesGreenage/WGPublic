3207cc94cb9a2ce0e869c4de3afba89f5c7aa4f9
WellesGreenage <70760609+WellesGreenage@users.noreply.github.com>
2020-09-05 18:29:46 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,43 +1,14 @@
 <!DOCTYPE html>
 <html>
 
-	<head>
-		<title>My site</title>
-	</head>
+    <head>
+        <title>My site</title>
+    </head>
 
-
-
-	<body>
-		<?php
-		echo 'Hello, world!';//Вывод
-		?></br>
-		<?php
-		$a = 100;
-		echo $a;
-		$b = array(1, 2, 3, 4, 5);
-		?></br>
-		<?php
-		echo $b[2];
+    <body>
+        <?php
+        echo 'Hello, world!';//Р’С‹РІРѕРґ
 		?>
-		<?php
-			$w = 'Rac';
-			$e = 'War';
-			echo $w.' '.$e;
-		?><br>
-		<?php
-			$w= 5;
-			$t = 7;
-			if (2+2 == 5){
-				echo $w;
-			} else 
-			{ echo $t;
-		};
-		?>
-		<?php
-		for($i = 0; $i <= 10; $i++)
-		{echo 'Hello<br>';}
-		?>
-
-	</body>
+        </body>
 
 </html>
