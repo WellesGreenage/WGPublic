8054afb07a6e16f27f933b882d45f96cff049f48
WellesGreenage <maletin.nikolaj@mail.ru>
2020-09-10 19:02:23 +0700

    Page cleanup
    Signed-off-by: WellesGreenage <maletin.nikolaj@mail.ru>

--- index.php
+++ index.php
@@ -8,59 +8,7 @@
 
 <body>
 <?php
-    echo 'Hello, world! I like java! Today is: ';
+    echo 'Hello, world! I like java!';
 ?>
-<?php
-date_default_timezone_set('Asia/Novosibirsk');
-echo date("D, j F Y, G:i");
-echo "<H1>My First project</H1>";
-echo "<br>";
-echo "<br>";
-echo "Output of a 10x11 table with numbers:";
-?>
-<table>
-<?php
-$r = 1;
-for ($i = 1; $i <= 10; $i++) {
-    echo "<tr>";
-    for ($k = 1; $k <= 11; $k++) {
-        echo "<td>$r</td>";
-        $r++;
-    }
-    echo "</tr>";
-}
-
-?>
-</table>
-<?php
-echo $r;
-echo "<br>";echo "<br>";
-echo '<table><form method="POST">';
-echo '<tr>
-        <td>Enter your name: </td><td><input name = "name" />
-        </td><td></td><td>Your name is: </td>
-        <td>';
-echo htmlspecialchars($_POST['name']);
-        echo '</td>
-      </tr>
-      <tr>
-        <td>Enter your surname: </td><td><input name = "sname" />
-        </td><td><button>Output</button></td><td>You surname is: </td>
-        <td>';
-echo htmlspecialchars($_POST['sname']);
-        echo '</td>
-      </tr>
-      <tr>
-        <td>Enter your Father name: </td><td><input name = "fname" />
-        </td><td></td><td>You Father name is: </td>
-        <td>';
-echo htmlspecialchars($_POST['fname']);
-        echo '</td>
-      </tr>
-      ';
-echo '</form></table>';
-//var_dump($_POST);
-?>
-
 </body>
 </html>
