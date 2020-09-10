9528f59b1282df6c78b4e529c4194455d4478543
WellesGreenage <maletin.nikolaj@mail.ru>
2020-09-10 21:39:39 +0700

    Added table output by parameters
    Signed-off-by: WellesGreenage <maletin.nikolaj@mail.ru>

--- index.php
+++ index.php
@@ -8,7 +8,36 @@
 
 <body>
 <?php
-    echo 'Hello, world! I like java!';
+    echo 'Hello, world! I like java!</br></br></br></br>';
+?>
+<?php
+echo '<form method="post">';
+echo '<input type="text" name="width" placeholder="Enter width"></br>';
+echo '<input type="text" name="height" placeholder="Enter height"></br>';
+echo '<button>Output</button></br></br>';
+echo '</form>';
+if (isset($_POST)) {
+    echo '<table>';
+    $r = 1;
+    for ($i = 1; $i <= $_POST['width']; $i++) {
+        echo "<tr>";
+        for ($k = 1; $k <= $_POST['height']; $k++) {
+            echo "<td>$r</td>";
+            $r++;
+        }
+        echo "</tr>";
+    }
+    echo '</table>';
+}
+if (empty($_POST['width']) or empty($_POST['height'])) {
+    exit ("Enter 2 number");
+}
+    echo '</br>';
+echo 'You entered the width:';
+echo $_POST['width'];
+    echo '</br>';
+echo 'You entered the height:';
+echo $_POST['height'];
 ?>
 </body>
 </html>
