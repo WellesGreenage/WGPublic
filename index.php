1b417f925e23ec151ad89f54af8230f33968b3bf
WellesGreenage <maletin.nikolaj@mail.ru>
2020-09-20 15:50:48 +0700

    Error fixed 2.0
    Signed-off-by: WellesGreenage <maletin.nikolaj@mail.ru>

--- index.php
+++ index.php
@@ -20,39 +20,69 @@ ob_start();
 
 <body>
 <?php
-    echo 'Hello, world! I like java!</br></br></br></br>';
+echo 'Hello, world! I like java! Today is: ';
 ?>
 <?php
-echo '<form method="post">';
-echo '<input type="text" name="width" placeholder="Enter width"></br>';
-echo '<input type="text" name="height" placeholder="Enter height"></br>';
-echo '<button>Output</button></br></br>';
-echo '</form>';
-if (isset($_POST['width']) && ($_POST['height'])) {
-    echo '<table>';
+date_default_timezone_set('Asia/Novosibirsk');
+echo date("D, j F Y, G:i");
+echo "<H1>My First project</H1>";
+echo "<br>";
+echo "<br>";
+echo "Output of a 10x11 table with numbers:";
+?>
+<table>
+    <?php
     $r = 1;
-    for ($i = 1; $i <= $_POST['width']; $i++) {
+    for ($i = 1; $i <= 10; $i++) {
         echo "<tr>";
-        for ($k = 1; $k <= $_POST['height']; $k++) {
+        for ($k = 1; $k <= 11; $k++) {
             echo "<td>$r</td>";
             $r++;
         }
         echo "</tr>";
     }
-    echo '</table>';
-}
-if (empty($_POST['width']) or empty($_POST['height'])) {
-    exit ("Enter 2 number");
-}
-    echo '</br>';
-echo 'You entered the width:';
-echo $_POST['width'];
-    echo '</br>';
-echo 'You entered the height:';
-echo $_POST['height'];
+
+    ?>
+</table>
+<?php
+echo $r;
+echo "<br>";echo "<br>";
+echo '<table><form method="POST">';
+echo '<tr>
+    <td>Enter your name: </td><td><input name = "name" />
+    </td><td></td><td>Your name is: </td>
+    <td>';
+if (!empty($_POST["name"]))
+{echo htmlspecialchars($_POST['name']);}
+    else {echo "You have not entered a name!";}
+echo '</td>
+  </tr>
+  <tr>
+    <td>Enter your surname: </td><td><input name = "sname" />
+    </td><td><button>Output</button></td><td>You surname is: </td>
+    <td>';
+if (!empty($_POST["sname"]))
+{echo htmlspecialchars($_POST['sname']);}
+else {echo "You have not entered a surname!";}
+echo '</td>
+  </tr>
+  <tr>
+    <td>Enter your Father name: </td><td><input name = "fname" />
+    </td><td></td><td>You Father name is: </td>
+    <td>';
+if (!empty($_POST["sname"]))
+{echo htmlspecialchars($_POST['fname']);}
+else {echo "You have not entered a Father name!";}
+echo '</td>
+  </tr>
+  ';
+echo '</form></table>';
+//var_dump($_POST);
 ?>
+
 </body>
 </html>
+
 
 <?php
 ob_end_flush();
